f6896573cc94dbf8e9f7e2ff4f1d2fdded8c0928
Heron Singarajah <HESI@syntronic.com>
2020-09-20 17:06:34 -0400

    added new case data for each day of processing

--- processData.py
+++ processData.py
@@ -35,10 +35,13 @@ dateString = datetime.now().strftime("%A %B %d, %Y").replace(" 0", " ")
 localDataFilename = 'OntarioCovid19Data.csv'
 
 
-covidData = pd.read_csv(localDataFilename)
-
+covidData = pd.read_csv(localDataFilename, parse_dates=[2,3,4])
 covidDf = pd.DataFrame(covidData, columns=['Case_Reported_Date', 'Age_Group','Case_AcquisitionInfo','Outbreak_Related','Outcome1', 'Reporting_PHU','Reporting_PHU_City'])
 
+
+#times
+pdToday = pd.to_datetime('today').floor('D')
+pdYesterday = pdToday + pd.offsets.Day(-1)
 #city specific data
 TorontoOnly = covidDf[ covidDf['Reporting_PHU'] == TorontoPH]
 OttawaOnly = covidDf[ covidDf['Reporting_PHU'] == OttawaPH]
@@ -49,11 +52,21 @@ torontoActive = TorontoOnly[ TorontoOnly['Outcome1'] == "Not Resolved"]
 ottawaActive = OttawaOnly[ OttawaOnly['Outcome1'] == "Not Resolved"]
 durhamActive = DurhamOnly [DurhamOnly['Outcome1'] == "Not Resolved"]
 
+torontoNew = TorontoOnly[ TorontoOnly['Case_Reported_Date'] == pdYesterday]
+ottawaNew = OttawaOnly[ OttawaOnly['Case_Reported_Date'] == pdYesterday]
+durhamNew = DurhamOnly [DurhamOnly['Case_Reported_Date'] == pdYesterday]
+
 
 print (ottawaActive)
-print ("Active COVID19 Cases in Toronto: ", torontoActive.shape[0])
-print ("Active COVID19 Cases in Durham: ",  durhamActive.shape[0])
-print ("Active COVID19 Cases in Ottawa: ",  ottawaActive.shape[0])
+
+print ("New COVID19 Cases in Toronto:\t", torontoNew.shape[0], "\t   Active cases: ", torontoActive.shape[0])
+print ("New COVID19 Cases in Durham:\t",  ottawaNew.shape[0], "\t   Active cases: ", durhamActive.shape[0])
+print ("New COVID19 Cases in Ottawa:\t",  durhamNew.shape[0], "\t   Active cases: ", ottawaActive.shape[0])
+
+
+#print (covidDf.dtypes)
+#print (pdToday)
+#print (pdYesterday)
 #print (pd.DataFrame(covidData, columns=['Case_Reported_Date','Outcome1', 'Reporting_PHU']).tail(10))
 
 
